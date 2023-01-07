8957c9721599d1aabd85b0493c3d7bdb877ec23a
JakeTodoroki <taco7897@gmail.com>
2023-01-07 16:04:38 -0700

    fake data

---
+++ data/skills-data.js
@@ -0,0 +1,12 @@
+const Skills = [
+  {text: 'Kickflip', done: true},
+  {text: 'No-Comply Shuvit', done: false},
+  {text: 'Switch Kickflip', done: true},
+  {text: 'Back-Side Heelflip', done: true},
+  {text: 'dolphin flip', done: false},
+  {text: 'Frontside Popshuvit', done: true}
+]
+
+export {
+  skills
+}
